24e3576f7c5c4ad25f89506bcadc3462aef779cd
Godpu <908662421@qq.com>
2020-01-25 14:10:09 +0800

    update: sh script

--- servers/docker-python/go.sh
+++ servers/docker-python/go.sh
@@ -1,12 +1,22 @@
+# 这个文件是在宿主机上测试部属爬虫
 # 安装Chrome
 apt-get install libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# 安装chrome
+if [ -d "$(pwd)${google-chrome-stable_current_amd64.deb}" ]; then
+    echo "下载Chromedriver"
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+fi
 apt-get install -f
 dpkg -i google-chrome-stable_current_amd64.deb
 # 安装chromedriver
-wget -N https://npm.taobao.org/mirrors/chromedriver/80.0.3987.16/chromedriver_linux64.zip
+if [ -d "$(pwd)${chromedriver_linux64.zip}" ]; then
+    echo "下载Chromedriver"
+    wget -N https://npm.taobao.org/mirrors/chromedriver/80.0.3987.16/chromedriver_linux64.zip
+fi
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
+rm /usr/local/bin/chromedriver
+rm /usr/bin/chromedrive
 mv -f chromedriver /usr/local/share/chromedriver
 ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 ln -s /usr/local/share/chromedriver /usr/bin/chromedrive
