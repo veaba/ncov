411466e45a6eefb910acecd303b8f613b9fa240b
Godpu <908662421@qq.com>
2020-03-01 15:07:49 +0800

    update config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const ioServer = 'http://127.0.0.1:9999';
-// export const ioServer = 'https://2019-ncov.datav.ai';
+// export const ioServer = 'http://127.0.0.1:9999';
+export const ioServer = 'https://2019-ncov.datav.ai';
